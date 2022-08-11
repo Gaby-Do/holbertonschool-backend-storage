70515f80857301732d1cd6c3b64211298f3d0af8
root <eme.gaby.do@gmail.com>
2022-08-11 23:49:37 +0000

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,6 @@
 ---QL script that ranks country origins of bands, ordered by
 ---the number of (non-unique) fans
-SELECT origin, sum(fans) as nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+SELECT origin, sum(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
